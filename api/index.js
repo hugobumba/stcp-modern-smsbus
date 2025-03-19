e1489cadedc2d9297821c7a0a96941c97fb95289
Hugo <40358@alunos.upt.pt>
2025-03-19 05:23:56 +0000

    Cors added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +13,7 @@ const __dirname = path.dirname(__filename);
 
 const url = 'https://www.stcp.pt/pt/widget/post.php?uid=d72242190a22274321cacf9eadc7ec5f';
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, '../interface/build')));
 
 async function getHorarios(paragem) {
